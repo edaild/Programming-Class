c859865e2f0d41ec06939da4cfee2663f81940e8
Yoonho Jeong <109999749+edaild@users.noreply.github.com>
2024-04-16 14:59:53 +0900

    Add files via upload

---
+++ 정윤호_137064/4-7.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.Video;
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        for (int m='°¡'; m<= 'ÆR'; m++)
+        {
+            Debug.Log((char)m);
+        }
+  
+     }
+    }
+       
+ 
+
